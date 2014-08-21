f346f03ff1620e4d4acde2b1b38b2d655e896e81
lost_mohican <erkinun@gmail.com>
2014-08-21 21:27:53 +0300

    insert implemented on kdtree

--- src/main/java/KdTree.java
+++ src/main/java/KdTree.java
@@ -19,8 +19,12 @@ public class KdTree {
     }
     public void insert(Point2D p) {
         // add the point p to the set (if it is not already in the set)
-        throw new UnsupportedOperationException("Not Implemented");
+
+        int depth = 0;
+
+        root = insert(root, p, depth);
     }
+
     public boolean contains(Point2D p) {
         // does the set contain the point p?
         throw new UnsupportedOperationException("Not Implemented");
@@ -43,5 +47,49 @@ public class KdTree {
         private RectHV rect;    // the axis-aligned rectangle corresponding to this node
         private Node lb;        // the left/bottom subtree
         private Node rt;        // the right/top subtree
+    }
+
+
+    private Node createNode(Point2D p) {
+        Node nd = new Node();
+        nd.p = p;
+        nd.lb = null;
+        nd.rt = null;
+
+        size++;
+
+        return nd;
+    }
+
+    private Node insert(Node n, Point2D point2D, int depth) {
+
+        if (n == null) {
+            return createNode(point2D);
+        }
+
+        int nextDepth = depth + 1;
+
+        if (depth % 2 == 0) {
+            //check x coordinate
+
+            int xComp = Double.compare(n.p.x(), point2D.x());
+            insertInner(n, point2D, nextDepth, xComp);
+        }
+        else {
+            //check y coordinate
+            int yComp = Double.compare(n.p.y(), point2D.y());
+            insertInner(n, point2D, nextDepth, yComp);
+        }
+
+        return n;
+    }
+
+    private void insertInner(Node n, Point2D point2D, int nextDepth, int comp) {
+        if (comp > 0) {
+            n.lb = insert(n.lb, point2D, nextDepth);
+        }
+        else if (comp < 0) {
+            n.rt = insert(n.rt, point2D, nextDepth);
+        }
     }
 }
