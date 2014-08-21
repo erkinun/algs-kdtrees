c706fa991364e692aeb2c758ff657a2e1eca6bc7
erkin.unlu <erkin.unlu@monitise.com>
2014-08-21 15:50:51 +0300

    tests and KdTree introduced

---
+++ src/test/java/KdTreeTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KdTreeTest {
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testDraw() throws Exception {
+
+    }
+
+    @Test
+    public void testRange() throws Exception {
+
+    }
+
+    @Test
+    public void testNearest() throws Exception {
+
+    }
+}
